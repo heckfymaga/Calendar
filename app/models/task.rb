3393577fbdeffdc9c0105c42bd85d9d2a8bb4008
Ermolaev Ruslan <heckfy-ermolaev@yandex.ru>
2017-12-12 13:06:48 +0300

    blago

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,5 @@ class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   validates :user_id, :target, :date, presence: true
-  scope :today_tasks, ->(user, date) { ActiveRecord::Base.connection.execute("SELECT * FROM tasks WHERE (user_id = '#{user.id}') AND ((date = '#{date}') OR (frequency_id = 2) OR (frequency_id = 3 AND strftime('%w',date) = '#{date.wday}') OR (frequency_id = 4 AND cast(strftime('%d',date) as real) = '#{date.day}') OR (frequency_id = 5 AND cast(strftime('%d',date) as real) = '#{date.day}' AND cast(strftime('%m',date) as real) = '#{date.month}'))")}
+  scope :today_tasks, ->(user, date) { ActiveRecord::Base.connection.execute("SELECT * FROM tasks WHERE (user_id = '#{user.id}') AND ((date = '#{date}') OR (frequency_id = 2) OR (frequency_id = 3 AND extract(dow from timestamp date) = '#{date.wday}') OR (frequency_id = 4 AND extract(day from timestamp date) = '#{date.day}') OR (frequency_id = 5 AND extract(day from timestamp date) = '#{date.day}' AND extract(month from timestamp date) = '#{date.month}'))")}
 end
